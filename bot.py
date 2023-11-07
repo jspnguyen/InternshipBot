7706b937b080206446853696b46f3ae140bdc10f
Joseph Nguyen <theantisine@gmail.com>
2023-11-06 19:14:28 -0800

    Version 1.0

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import discord, json, requests, asyncio, datetime, time
+import discord, json, requests, asyncio, scrapers
 from discord import app_commands
-from bs4 import BeautifulSoup
 
 with open('vars/config.json', 'r') as f:
     data = json.load(f)
@@ -24,61 +23,19 @@ bot = pre_bot()
 tree = app_commands.CommandTree(bot)
 
 @tree.command(name="dailysearch", description="Search for new internship postings in the past 24 hours", guild=discord.Object(id=TEST_SERVER)) 
-async def self(interaction: discord.Interaction):
-    # Psuedocode
-    # Scrape from LinkedIn
-    # Send each listing with a 0.25 second break in-between
-    # Scrape from Levels
-    # Send each listing with a 0.25 second break in-between
-    # Scrape from GitHub
-    # Send each listing with a 0.25 second break in-between
-    # Each listing should be an embed with the Company, Role, Location, and Link to Apply
-    # Edge Cases: Site ban, Position is locked on github
-        
+async def self(interaction: discord.Interaction):   
     page_data = requests.get(links["github24"])
     
     await interaction.response.defer()
-    await asyncio.sleep(10) # May need to edit
+    await asyncio.sleep(10) # ? Needs adjusting
 
-    result = BeautifulSoup(page_data.text, 'lxml')
-    table_data = str(result.find("table"))
-
-    table_data = table_data.replace("</tr>", "")
-    table_data = table_data.replace("</tbody>", "")
-    table_data = table_data.replace("</table>", "")
-    table_data = table_data.replace("</td>", "")
-    table_data = table_data.replace("\n", "")
-    table_data = table_data.split("<tr>")
-
-    for entry in table_data:
-        job_data = entry.split("<td>")
-        if job_data[-1] == "Nov 06":
-            link_split = job_data[4].split('"')
-            
-            location_split = job_data[3].replace("<details><summary><strong>", "")
-            location_split = location_split.replace("</details>", " ")
-            location_split = location_split.replace("</strong></summary>", "/")
-            location_split = location_split.replace("<br/>", "/")
-            location_split = location_split.split("/")
-            
-            role_title = job_data[2]
-            if len(location_split) == 1:
-                location_title = location_split[0]
-            else:
-                location_string = ""
-                for i in range(1, len(location_split)):
-                    if i == len(location_split) - 1:
-                        location_string += location_split[i]
-                    else:
-                        location_string += location_split[i] + ", "
-                location_title = location_string
-            app_link = link_split[1]
-            date_posted = job_data[5]
-            
-            embed = discord.Embed(title=f'{role_title}', description=f"{location_title}", color=discord.Colour.green())
-            embed.add_field(name=f"Apply", value=f"{app_link}")
-            embed.set_footer(text=f"{date_posted}")
-            await interaction.channel.send(embed=embed)
+    response = scrapers.githubData(page_data)
+    
+    for listing in response:
+        embed = discord.Embed(title=f'{listing[0]}', description=f"{listing[1]}", color=discord.Colour.green())
+        embed.add_field(name=f"Apply", value=f"{listing[2]}")
+        embed.set_footer(text=f"{listing[3]}")
+        await interaction.channel.send(embed=embed)
             
     await interaction.followup.send("Finished sending")
 
@@ -89,6 +46,7 @@ async def self(interaction: discord.Interaction):
 @tree.command(name="help", description="Shows commands for the bot", guild=discord.Object(id=TEST_SERVER)) 
 async def self(interaction: discord.Interaction):
     embed = discord.Embed(title="Commands", description="All bot commands", color=discord.Colour.gold())
+    embed.add_field(name=f"/dailysearch", value=f"Shows today's internship postings according to Pitts GitHub repo")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 if __name__ == '__main__':
---
+++ scrapers.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+
+def githubData(page_data: requests.models.Response) -> list:
+    return_list = []
+    
+    now = datetime.now()
+    current_date = now.strftime("%b") + " " + now.strftime("%d")
+    
+    result = BeautifulSoup(page_data.text, 'lxml')
+    table_data = str(result.find("table"))
+
+    table_data = githubClean(table_data)
+
+    for entry in table_data:
+        job_data = entry.split("<td>")
+        if job_data[-1] == current_date:
+            link_split = job_data[4].split('"')
+            
+            location_split = locationClean(job_data[3])
+            
+            role_title = job_data[2]
+            if len(location_split) == 1:
+                location_title = location_split[0]
+            else:
+                location_string = ""
+                for i in range(1, len(location_split)):
+                    if i == len(location_split) - 1:
+                        location_string += location_split[i]
+                    else:
+                        location_string += location_split[i] + ", "
+                location_title = location_string
+            app_link = link_split[1]
+            date_posted = job_data[5]
+            
+            return_list.append([role_title, location_title, app_link, date_posted])
+    return return_list
+
+def githubClean(table_data: str) -> list:
+    table_data = table_data.replace("</tr>", "")
+    table_data = table_data.replace("</tbody>", "")
+    table_data = table_data.replace("</table>", "")
+    table_data = table_data.replace("</td>", "")
+    table_data = table_data.replace("\n", "")
+    table_data = table_data.split("<tr>")
+    
+    return table_data
+
+def locationClean(location_data: str) -> list:
+    location_split = location_data.replace("<details><summary><strong>", "")
+    location_split = location_split.replace("</details>", " ")
+    location_split = location_split.replace("</strong></summary>", "/")
+    location_split = location_split.replace("<br/>", "/")
+    location_split = location_split.split("/")
+    
+    return location_split
