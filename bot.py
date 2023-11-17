09fe26ccb3a88559d7932091bd0a92b2f5aa74ba
Joseph Nguyen <theantisine@gmail.com>
2023-11-16 22:26:14 -0800

    added new details and commands

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-import discord, json, requests, asyncio, scrapers, time, pytz
+import discord, json, requests, asyncio, time, pytz
 from discord import app_commands
 from datetime import datetime, timedelta
 from discord.ext import tasks
+import scrapers
 
 with open('vars/config.json', 'r') as f:
     data = json.load(f)
@@ -27,6 +28,7 @@ class pre_bot(discord.Client):
 bot = pre_bot()
 tree = app_commands.CommandTree(bot)
 
+# TODO: Use company names
 @tree.command(name="dailysearch", description="Search for new internship postings in the past 24 hours") 
 @app_commands.describe(style="Choose a method for displaying the roles")
 @app_commands.choices(style=[
@@ -40,7 +42,7 @@ async def self(interaction: discord.Interaction, style: discord.app_commands.Cho
         await interaction.response.defer()
         await asyncio.sleep(10) # ? Needs adjusting
 
-        response = scrapers.githubData(page_data)
+        response = scrapers.githubData(page_data, 0)
         
         if style.value == 2:
             for listing in response:
@@ -58,7 +60,7 @@ async def self(interaction: discord.Interaction, style: discord.app_commands.Cho
             for listing in response:
                 embed.add_field(name=f"{listing[0]}", value=f"{listing[2]}", inline=False)
             
-            #TODO: Implement pagination for embed
+            #TODO: Implement pagination for embed?
             await interaction.channel.send(embed=embed)
                 
         await interaction.followup.send("Finished.")
@@ -78,9 +80,69 @@ async def self(interaction: discord.Interaction):
     embed = discord.Embed(title="Subscribed!", color=discord.Colour.dark_green())
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
-# @tree.command(name="searchweekly", description="Search for new internship postings in the past week") 
-# async def self(interaction: discord.Interaction):
-#     return
+@tree.command(name="weeklysearch", description="Search for new internship postings in the past 7 hours") 
+@app_commands.describe(style="Choose a method for displaying the roles")
+@app_commands.choices(style=[
+    discord.app_commands.Choice(name="Compact", value=1),
+    discord.app_commands.Choice(name="Singular", value=2),
+])
+async def self(interaction: discord.Interaction, style: discord.app_commands.Choice[int]):   
+    try:
+        page_data = requests.get(links["GITHUB_24"])
+        
+        await interaction.response.defer()
+        await asyncio.sleep(10) # ? Needs adjusting
+
+        response = scrapers.githubData(page_data, 1)
+        
+        if style.value == 2:
+            for listing in response:
+                embed = discord.Embed(title=f'{listing[0]}', description=f"{listing[1]}", color=discord.Colour.green())
+                embed.add_field(name=f"Apply", value=f"{listing[2]}")
+                embed.set_footer(text=f"{listing[3]}")
+                
+                time.sleep(0.25)
+                await interaction.channel.send(embed=embed)
+        elif style.value == 1:
+            current_time_pst = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pst_timezone)
+            formatted_date = current_time_pst.strftime('%m/%d/%y')
+            
+            embed = discord.Embed(title=f'Daily Internships', description=f"{formatted_date}", color=discord.Colour.green())
+            for listing in response:
+                embed.add_field(name=f"{listing[0]}", value=f"{listing[2]}", inline=False)
+            
+            #TODO: Implement pagination for embed?
+            await interaction.channel.send(embed=embed)
+                
+        await interaction.followup.send("Finished.")
+    except:
+        embed = discord.Embed(title="Please try again.", color=discord.Colour.red())
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+@tree.command(name="search", description="Search for new internship postings based on a keyword") 
+async def self(interaction: discord.Interaction, term: str):
+    try:
+        page_data = requests.get(links["GITHUB_24"])
+        
+        await interaction.response.defer()
+        await asyncio.sleep(10) # ? Needs adjusting
+
+        response = scrapers.githubData(page_data, 0)
+        
+        current_time_pst = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pst_timezone)
+        formatted_date = current_time_pst.strftime('%m/%d/%y')
+        
+        embed = discord.Embed(title=f'Daily Internships', description=f"{formatted_date}", color=discord.Colour.green())
+        for listing in response:
+            if term in listing[0]:
+                embed.add_field(name=f"{listing[0]}", value=f"{listing[2]}", inline=False)
+        
+        await interaction.channel.send(embed=embed)
+                
+        await interaction.followup.send("Finished.")
+    except:
+        embed = discord.Embed(title="Please try again.", color=discord.Colour.red())
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 @tree.command(name="invite", description="Invite the bot to your server!") 
 async def self(interaction: discord.Interaction):
@@ -93,6 +155,7 @@ async def self(interaction: discord.Interaction):
     embed.add_field(name=f"/invite", value=f"Sends invite link for the bot")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
+# TODO: Subscription part is broken
 @tasks.loop(hours=24)
 async def daily_update():
     for channel_id in channels.values():
--- scrapers.py
+++ scrapers.py
@@ -1,23 +1,33 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests, pytz, re
 
 pst = pytz.timezone('America/Los_Angeles')
 
-def githubData(page_data: requests.models.Response) -> list:
+def githubData(page_data: requests.models.Response, timeframe: int) -> list:
     return_list = []
     
+    search_dates = []
     now = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pst)
-    current_date = now.strftime("%b %d")
+
+    if timeframe == 1:
+        for i in range(7):
+            date = now - timedelta(days=i)
+            formatted_date = date.strftime("%b %d")
+            search_dates.append(formatted_date)
+    else:
+        search_dates.append(now.strftime("%b %d"))
     
     result = BeautifulSoup(page_data.text, 'lxml')
     table_data = str(result.find("table"))
 
     table_data = githubClean(table_data)
 
+    prev_company_name = ""
+    
     for entry in table_data:
         job_data = entry.split("<td>")
-        if job_data[-1] == current_date:
+        if job_data[-1] in search_dates:
             link_split = job_data[4].split('"')
             
             location_split = locationClean(job_data[3])
@@ -33,10 +43,24 @@ def githubData(page_data: requests.models.Response) -> list:
                     else:
                         location_string += location_split[i] + ", "
                 location_title = location_string
-            app_link = link_split[1]
+                
+            try:
+                app_link = link_split[1]
+            except: 
+                app_link = "LOCKED"
             date_posted = job_data[5]
             
-            return_list.append([role_title, location_title, app_link, date_posted])
+            if "href" in job_data[1]:
+                match = re.search(r'rel="nofollow">(.*?)</a>', job_data[1])
+                company_name = match.group(1)
+                prev_company_name = company_name
+            elif "↳" in job_data[1]:
+                company_name = prev_company_name
+            else:
+                company_name = job_data[1]
+                prev_company_name = company_name
+            
+            return_list.append([role_title, location_title, app_link, date_posted, company_name])
     return return_list
 
 def githubClean(table_data: str) -> list:
